e3b402263e71ea93ace40249ff80377feb3ecad1
ust <ust@t21-dit-0034409>
2017-04-28 09:09:58 +0300

    6.10

--- src/_00_main/Main.java
+++ src/_00_main/Main.java
@@ -1,16 +1,12 @@
 package _00_main;
 
-import _06_polymorphism_09_extensibility.*;
+import _06_polymorphism_10_extensibility.*;
 
 public class Main
 {
     public static void main(String[] args) 
     {
-        Rodent[] rodents = new Rodent[10];
-        for (int i = 0; i < rodents.length; i++)
-            rodents[i] = Generator.next();
-        
-        for(Rodent i : rodents)
-            System.out.println(i);
-    }
+        Rodent rodent = new Mouse();
+		rodent.print();
+	}
 }
