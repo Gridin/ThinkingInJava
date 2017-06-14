7e4a0ef1406144042c314c650b0f77d96e5e447e
Maksim Gridin <m.gridin@favbet.com>
2017-06-14 11:26:24 +0300

    commit

--- src/_08_inner_classes_21_classes_inside_interfaces/MyMain.java
+++ src/_08_inner_classes_21_classes_inside_interfaces/MyMain.java
@@ -4,7 +4,38 @@ public class MyMain
 {
     public static void myMain()
     {
+        int x = 0;
         
+        while (x < 4) //
+        {
+            x = x - 1; 
+                    
+            if (x < 1)
+            {
+                System.out.print("a "); 
+            }
+
+            x = x + 2;
+            
+            if (x > 3) //
+            {
+                System.out.print("an"); 
+                System.out.print(" oyster");
+            }
+            
+            if (x == 1)
+            {
+                System.out.print("noise");
+            }
+            
+            if (x > 1) //
+            {
+                System.out.print("annoys");
+            }
+            
+            System.out.println("");
+            x = x + 1;
+        }                
     }
     
 }
